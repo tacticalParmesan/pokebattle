d7959e5312d09c2e8004b4bd57d6630f7a4ae957
tacticalParmesan <edoardoferreri10@gmail.com>
2024-02-17 14:13:33 +0100

    Fixed assets

--- fecthData.js
+++ fecthData.js
@@ -1,5 +1,5 @@
-import data from "/pokebattle/pokemondata.json" assert { type: "json" };
-import movesData from "/pokebattle/moves.json" assert { type: "json" };
+import data from "./pokemondata.json" assert { type: "json" };
+import movesData from "./moves.json" assert { type: "json" };
 
 export function getEnemyPokemon(pokemonName) {
     return data[pokemonName]
